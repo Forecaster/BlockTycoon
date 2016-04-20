0f551dc1a7a8e8284e422db6afc652ab744ea2f8
Forecaster <martinek90@hotmail.com>
2016-04-20 22:01:01 +0200

    You know, setup stuff, kinda?

---
+++ src/main/java/org/towerofawesome/blockTycoon.java
@@ -0,0 +1,16 @@
+package org.towerofawesome;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+
+/**
+ * Created by Forecaster on 20/04/2016.
+ */
+@Mod(modid = "blockTycoon", version = "1.0.0.0")
+public class blockTycoon
+{
+  public void init(FMLInitializationEvent event)
+  {
+
+  }
+}
