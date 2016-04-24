eaecffe896589d0e63557985acfe81b8692fd6fd
Vexatos <Stuarzt@gmx.de>
2016-04-24 12:18:14 +0200

    Fixed logger and block initialization.

--- src/main/java/org/towerofawesome/init/ModBlocks.java
+++ src/main/java/org/towerofawesome/init/ModBlocks.java
@@ -2,6 +2,7 @@ package org.towerofawesome.init;
 
 import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.block.material.Material;
+import org.towerofawesome.BlockTycoon;
 import org.towerofawesome.References;
 import org.towerofawesome.block.BlockGenerator;
 
@@ -10,12 +11,15 @@ import org.towerofawesome.block.BlockGenerator;
  */
 public class ModBlocks
 {
-  private static final BlockGenerator generator = new BlockGenerator("generator", Material.iron);
+  private static BlockGenerator generator;
 
   public static void init()
   {
-    References.log("Registering blocks!");
+    BlockTycoon.log.info("Registering blocks!");
+
+    generator = new BlockGenerator("generator", Material.iron);
     GameRegistry.registerBlock(generator, generator.getUnlocalizedName());
-    References.log("Register " + generator.getUnlocalizedName());
+
+    BlockTycoon.log.info("Register " + generator.getUnlocalizedName());
   }
 }
